129a01da214774d9fc5ca732ee450fdeb33057fe
thisisshi <theli@outlook.com>
2019-02-03 21:41:14 -0500

    some updates

--- linux/.config/fish/config.fish
+++ linux/.config/fish/config.fish
@@ -1,12 +1,3 @@
-# Start X at login
-if status is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx -- -keeptty
-    end
-end
-
-set -gx OP_SESSION_my 4kIXaYcru_gr4nq76e27lTMxuMp1TTQQRbPlTPyp02g
-
 function fish_greeting
 end
 
@@ -22,9 +13,3 @@ end
 function ls --description "better ls"
 	command ls -Glha
 end
-
-function brightness --description "set brightness"
-  command xrandr --output eDP-1 --brightness $argv
-end
-
-alias wifi="sudo nmtui"
